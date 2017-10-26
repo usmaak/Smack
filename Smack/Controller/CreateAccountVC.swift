01319938b1eff41a3e5ce9ede8ee2eae42f06471
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-10-25 21:18:26 -0600

    lesson 12

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -14,6 +14,10 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,12 +34,31 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else {return}
         guard let email = emailTxt.text, emailTxt.text != "" else {return}
         guard let pass =  passTxt.text, passTxt.text != "" else {return}
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("Registered user")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                            else {
+                                debugPrint("Fail - 3")
+                            }
+                        })
+                    }
+                    else {
+                        debugPrint("Fail - 2")
+                    }
+                })
+            }
+            else {
+                debugPrint("Fail 1")
             }
         }
     }
--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -53,11 +53,7 @@ class AuthService {
         
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
-                AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
-                    if success {
-                        print("Logged in user!", AuthService.instance.authToken)
-                    }
-                })
+                completion(true)
             }
             else {
                 completion(false)
@@ -65,28 +61,101 @@ class AuthService {
             }
         }
     }
+
     
     func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
         let lowerCaseEmail = email.lowercased()
+        
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
-     
-        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
-            if response.result.error == nil {
-                //Using SwiftyJSON
-                guard let data = response.data else {return}
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).validate().responseJSON { (response) in
+            //if response.result.error == nil {
+                //                if let json = response.result.value as? Dictionary<String, Any> {
+                //                    if let email = json["user"] as? String {
+                //                        self.userEmail = email
+                //                    }
+                //                    if let token = json["token"] as? String {
+                //                        self.authToken = token
+                //                    }
+                //                }
+                //Using SwiftyJSON:
+                guard let data = response.data else { return }
                 let json = JSON(data: data)
                 self.userEmail = json["user"].stringValue
                 self.authToken = json["token"].stringValue
                 
+                
                 self.isLoggedIn = true
+                completion(true)
+//            } else {
+//                completion(false)
+//                debugPrint(response.result.error as Any)
+//            }
+        }
+    }
+
+    
+//    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+//        let lowerCaseEmail = email.lowercased()
+//        let body: [String: Any] = [
+//            "email": lowerCaseEmail,
+//            "password": password
+//        ]
+//
+//        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+//            if response.result.error == nil {
+//                //Using SwiftyJSON
+//                guard let data = response.data else {return}
+//                let json = JSON(data: data)
+//                self.userEmail = json["user"].stringValue
+//                self.authToken = json["token"].stringValue
+//
+//                self.isLoggedIn = true
+//                completion(true)
+//            }
+//            else {
+//                completion(false)
+//                debugPrint(response.result.error as Any)
+//            }
+//        }
+//    }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                let id = json["_id"].stringValue
+                let avatarColor = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, avatarColor: avatarColor, avatarName: avatarName, email: email, name: name)
                 completion(true)
             }
             else {
                 completion(false)
-                debugPrint(response.result.error as Any)
+                debugPrint("Screwed up!")
+                debugPrint("Failure at Alamofire Request: ", response.result.error as Any)
             }
         }
     }
